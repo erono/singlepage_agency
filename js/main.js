df4368641b31b6ece058db41ddb7652e95e3ae82
Emmanuel  Rono <erono@Emmanuels-MacBook-Pro.local>
2023-12-08 12:17:33 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// author: Emmanuel Rono;
+// phone Number: +254 716 030 906;
+// Email: kogzkip@gmail.com
+
+$(document).ready(function() {
+    $('nav a').click(function(){
+        $('nav a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
